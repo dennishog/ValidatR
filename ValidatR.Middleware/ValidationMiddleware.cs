8761235481d5d8a1f1c724eea2b76b6d25269ccf
Dennis Hogström <dennis.hogstrom@outlook.com>
2022-12-17 18:04:13 +0100

    Added empty middleware

---
+++ ValidatR.Middleware/ValidationMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ValidatR.Middleware;
+
+public class ValidationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ValidationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+
+
+        await _next(httpContext);
+    }
+}
