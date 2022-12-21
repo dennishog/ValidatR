1094ffd7a4ae5484fb2873af23cc40a350670588
Dennis Hogström <dennis.hogstrom@outlook.com>
2022-12-21 13:28:45 +0100

    Refact

--- ValidatR/Validators/ValidatorRule.cs
+++ ValidatR/Validators/ValidatorRule.cs
@@ -17,6 +17,20 @@ public abstract class ValidatorRule<TParameter> : IValidatorRule<TParameter>
     {
         var value = propertyInfo.GetValue(model);
 
+        var attribute = GetValidateAttribute<TModel>(propertyInfo);
+
+        if (attribute == null || !attribute.ValidatorType.HasFlag(ValidatorType))
+        {
+            return;
+        }
+
+        await ValidateAsync(attribute, value, GetValueFunc(attribute.Id, ValidatorType, parameter), cancellationToken);
+    }
+
+    protected abstract Task ValidateAsync<TProperty>(ValidateAttribute attribute, TProperty value, string validationValue, CancellationToken cancellationToken);
+
+    private ValidateAttribute? GetValidateAttribute<TModel>(PropertyInfo propertyInfo)
+    {
         var attribute = propertyInfo.GetCustomAttribute<ValidateAttribute>();
 
         // If attribute is not found, check constructor parameters if there is a ValidateAttribute declared
@@ -28,25 +42,13 @@ public abstract class ValidatorRule<TParameter> : IValidatorRule<TParameter>
                 continue;
             }
 
-            var parameterInfo = constructor.GetParameters().SingleOrDefault(x => x.Name == propertyInfo.Name);
+            var parameterInfo = constructor.GetParameters().SingleOrDefault(x => x.Name.Equals(propertyInfo.Name, StringComparison.OrdinalIgnoreCase));
             if (parameterInfo != null)
             {
                 attribute = parameterInfo.GetCustomAttribute<ValidateAttribute>();
             }
         }
 
-        if (attribute == null)
-        {
-            return;
-        }
-
-        if (!attribute.ValidatorType.HasFlag(ValidatorType))
-        {
-            return;
-        }
-
-        await ValidateAsync(attribute, value, GetValueFunc(attribute.Id, ValidatorType, parameter), cancellationToken);
+        return attribute;
     }
-
-    protected abstract Task ValidateAsync<TProperty>(ValidateAttribute attribute, TProperty value, string validationValue, CancellationToken cancellationToken);
 }
