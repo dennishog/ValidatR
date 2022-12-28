cd4393af67e25acd2b60c1f4b781014d84046add
Dennis Hogström <dennis.hogstrom@outlook.com>
2022-12-28 23:29:40 +0100

    Error

--- ValidatR.AspNet/ApplicationBuilderExtensions.cs
+++ ValidatR.AspNet/ApplicationBuilderExtensions.cs
@@ -5,7 +5,7 @@ public static class ApplicationBuilderExtensions
     public static IApplicationBuilder UseValidatorMiddleware<TModel>(this IApplicationBuilder self) where TModel : class
     {
         return self
-            .UseMiddleware<ValidationErrorMiddleware>()
+            .UseMiddleware<ValidationExceptionMiddleware>()
             .UseMiddleware<ValidationMiddleware<TModel>>();
     }
 }
