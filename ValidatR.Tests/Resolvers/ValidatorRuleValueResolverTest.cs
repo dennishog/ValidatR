cbd06923052a8cfdaea5c1907068990db3092c90
Dennis Hogström <dennis.hogstrom@outlook.com>
2022-12-30 22:42:10 +0100

    Added README as solution item

--- ValidatR.Tests/Resolvers/ValidatorRuleValueResolverTest.cs
+++ ValidatR.Tests/Resolvers/ValidatorRuleValueResolverTest.cs
@@ -1,6 +1,27 @@
-﻿namespace ValidatR.Tests.Resolvers;
+﻿using ValidatR.Resolvers;
+
+namespace ValidatR.Tests.Resolvers;
 
 public class ValidatorRuleValueResolverTest
 {
+    [Fact]
+    public void AddAndGetValidatorRuleValueSuccessfully()
+    {
+        var fixture = new Fixture();
+        var id = fixture.Create<string>();
+        var parameter = fixture.Create<string>();
 
+        var sut = new ValidatorRuleValueResolver<string>();
+
+        Func<string, string, string> func = (id, parameter) =>
+        {
+            return $"{id}{parameter}";
+        };
+
+        sut.AddRuleValueFunc(typeof(string), func);
+
+        var result = sut.GetValidatorRuleValue<string>(typeof(string), id, parameter);
+
+        result.Should().Be($"{id}{parameter}");
+    }
 }
