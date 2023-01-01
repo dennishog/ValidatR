e2ff081a1d67ea5f5ace479ae955e5c54c2b68ac
Dennis Hogström <dennis.hogstrom@outlook.com>
2023-01-01 13:19:09 +0100

    Added unit tests for dependencyinjection package

---
+++ ValidatR.DependencyInjection.Tests/ApplicationBuilderExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using NSubstitute;
+
+namespace ValidatR.DependencyInjection.Tests;
+
+public class ApplicationBuilderExtensionsTest
+{
+    [Fact]
+    public void UseValidatRSuccessfully()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddValidatR<string>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var applicationBuilder = Substitute.For<IApplicationBuilder>();
+        applicationBuilder.ApplicationServices.Returns(serviceProvider);
+
+        applicationBuilder.UseValidatR<string>()
+            .AddMinLengthValidator((id, parameter) => 1)
+            .AddMaxLengthValidator((id, parameter) => 10)
+            .AddRegexValidator((id, parameter) => @"\d\d")
+            .AddRequiredValidator((id, parameter) => true);
+    }
+}
---
+++ ValidatR.DependencyInjection.Tests/ServiceCollectionExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿using ValidatR.DependencyInjection.Tests.Fakes;
+using ValidatR.Providers;
+using ValidatR.Resolvers;
+using ValidatR.Validators;
+
+namespace ValidatR.DependencyInjection.Tests;
+
+public class ServiceCollectionExtensionsTest
+{
+    [Fact]
+    public void AddValidatRSuccessfully()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddValidatR<string>().AddParameterResolver<RequestModel>(x => x.StringValue!);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var validatorRuleValueResolver = serviceProvider.GetRequiredService<IValidatorRuleValueResolver<string>>();
+        validatorRuleValueResolver.Should().NotBeNull();
+
+        var propertyProvider = serviceProvider.GetRequiredService<IPropertyProvider>();
+        propertyProvider.Should().NotBeNull();
+
+        var validatorWithParameter = serviceProvider.GetRequiredService<IValidator<string>>();
+        validatorWithParameter.Should().NotBeNull();
+
+        var validator = serviceProvider.GetRequiredService<IValidator>();
+        validator.Should().NotBeNull();
+
+        var validatorRules = serviceProvider.GetRequiredService<IEnumerable<IValidatorRule<string>>>();
+        validatorRules.Should().NotBeNull();
+        validatorRules.Should().HaveCount(4);
+
+        var minLength = validatorRules.SingleOrDefault(validatorRule => validatorRule.GetType().Equals(typeof(MinLengthValidatorRule<string>)));
+        minLength.Should().NotBeNull();
+
+        var maxLength = validatorRules.SingleOrDefault(validatorRule => validatorRule.GetType().Equals(typeof(MaxLengthValidatorRule<string>)));
+        maxLength.Should().NotBeNull();
+
+        var regex = validatorRules.SingleOrDefault(validatorRule => validatorRule.GetType().Equals(typeof(RegexValidatorRule<string>)));
+        regex.Should().NotBeNull();
+
+        var required = validatorRules.SingleOrDefault(validatorRule => validatorRule.GetType().Equals(typeof(RequiredValidatorRule<string>)));
+        required.Should().NotBeNull();
+
+        var parameterResolver = serviceProvider.GetRequiredService<IParameterResolver<string>>();
+        parameterResolver.Should().NotBeNull();
+    }
+}
